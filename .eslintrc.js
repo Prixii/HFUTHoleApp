ba543c7471f633de3f1c61f79e0d4f44bffd005f
snowingfox <1503401882@qq.com>
2023-07-21 22:16:23 +0800

    legacy-peer-deps=true

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,6 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'warn',
         'react-hooks/exhaustive-deps': 'warn',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
   ],
