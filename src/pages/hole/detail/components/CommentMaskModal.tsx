a575c724ab67a13c71e12eaab43bc48f6cfbaa9a
wang.jiaqi <279298470@qq.com>
2023-09-14 10:41:42 +0800

    [fix]: 修复iOS下，评论弹窗无法关闭的bug

--- src/pages/hole/detail/components/CommentMaskModal.tsx
+++ src/pages/hole/detail/components/CommentMaskModal.tsx
@@ -2,15 +2,24 @@ import { useBottomCommentContext } from '@/shared/context/hole/comment'
 import { CommentInputForm } from '@/pages/hole/detail/components/CommentInputForm'
 import React from 'react'
 import { Modal } from 'native-base'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 import { useRoute } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export function CommentMaskModal() {
   const { showInput, closeInput } = useBottomCommentContext()
   const route = useRoute()
   const { id } = route.params as { id: number }
 
-  console.log('[id]' + id)
+  const renderTouchable = () => {
+    if (Platform.OS === 'ios') {
+      return (
+        <TouchableOpacity onPress={() => closeInput(true)}>
+          <View className="ocapcity-0 h-full"></View>
+        </TouchableOpacity>
+      )
+    }
+  }
   return (
     <>
       <Modal
@@ -19,6 +28,7 @@ export function CommentMaskModal() {
         animationPreset={'slide'}
       >
         <View className={'absolute left-0 right-0 top-0 bottom-0'}>
+          {renderTouchable()}
           <CommentInputForm onCommentSuccess={() => closeInput(true)} id={id} />
         </View>
       </Modal>
